dcb406ed4bc1b9644621b2f379524f4b2156165c
Joseph Luhandu <josephluhandu@yahoo.com>
2020-03-30 20:04:25 +0200

    halo

--- SimpleApi.test/UnitTest1.cs
+++ SimpleApi.test/UnitTest1.cs
@@ -7,13 +7,13 @@ namespace SimpleApi.test
     public class UnitTest1
     {
 
-        ValuesController controller = new ValuesController();
-        [Fact]
-        public void GetReturnMyName()
-        {
-            var returnValue = controller.Get(1);
-            Assert.Equal("Arobas Luhandu", returnValue.Value);
-        }
+        //ValuesController controller = new ValuesController();
+        //[Fact]
+        //public void GetReturnMyName()
+        //{
+        //    var returnValue = controller.Get(1);
+        //    Assert.Equal("Arobas Luhandu", returnValue.Value);
+        //}
         [Fact]
         public void Test1()
         {
